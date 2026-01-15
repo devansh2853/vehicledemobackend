977340af04a1df3a89a9e2b195a4131399d4db23
Devansh <devanshbansal2021@gmail.com>
2026-01-15 10:13:35 +0530

    Updated workflow 2

--- VehicleProject/Program.cs
+++ VehicleProject/Program.cs
@@ -29,7 +29,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-
 // using (var scope = app.Services.CreateScope())
 // {
 //     var db = scope.ServiceProvider.GetRequiredService<VehicleProjectContext>();
