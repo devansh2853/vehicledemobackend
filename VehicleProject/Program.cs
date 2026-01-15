a64aa05da5b61aa9c62e1e60e903cac3af7d3981
Devansh <devanshbansal2021@gmail.com>
2026-01-15 10:45:12 +0530

    Updated readme and removed comments from program.cs

--- VehicleProject/Program.cs
+++ VehicleProject/Program.cs
@@ -29,12 +29,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<VehicleProjectContext>();
-//     db.Database.Migrate();
-// }
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
