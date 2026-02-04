5b3cceb15103333e6b1b12ac3ceddd64d6127a04
Devansh <devanshbansal2021@gmail.com>
2026-02-05 04:37:34 +0530

    Truck migration

---
+++ VehicleProject/Migrations/20260204230310_TruckVehicleType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class TruckVehicleType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VehicleTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Truck" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VehicleTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
