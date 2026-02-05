ae8368358423765102285342335fdf34a8ce4d71
Devansh <devanshbansal2021@gmail.com>
2026-02-05 10:04:10 +0530

    Deletion logic in sql script

--- VehicleProject/SQL_queries/vehicle_types.sql
+++ VehicleProject/SQL_queries/vehicle_types.sql
@@ -3,8 +3,7 @@ SET IDENTITY_INSERT VehicleTypes ON;
 MERGE INTO VehicleTypes AS Target
 USING (VALUES 
     (1, 'Car'),
-    (2, 'Bus'),
-    (3, 'Cycle')
+    (2, 'Bus')
 ) AS Source (Id, Name)
 ON Target.Id = Source.Id
 
@@ -14,6 +13,10 @@ WHEN MATCHED AND Target.Name <> Source.Name THEN
 
 -- If the ID doesn't exist at all, insert it
 WHEN NOT MATCHED BY TARGET THEN
-    INSERT (Id, Name) VALUES (Source.Id, Source.Name);
+    INSERT (Id, Name) VALUES (Source.Id, Source.Name)
+
+-- If ID is in DB but not in script then delete it
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE;
 
 SET IDENTITY_INSERT VehicleTypes OFF;
