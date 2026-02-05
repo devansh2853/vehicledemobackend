8b5e13cc55651b8cfaeb84eb7bfc49fa73478151
Devansh <devanshbansal2021@gmail.com>
2026-02-05 07:09:21 +0530

    Added migration for seeding change

---
+++ VehicleProject/Migrations/20260205013856_ChangedDataSeedingMethod.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VehicleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedDataSeedingMethod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VehicleTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "VehicleTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "VehicleTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VehicleTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Car" },
+                    { 2, "Bus" },
+                    { 3, "Truck" }
+                });
+        }
+    }
+}
