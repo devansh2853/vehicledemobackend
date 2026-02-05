ff66d505bccdbe4ab7c3335f0385435fe5d6c134
Devansh <devanshbansal2021@gmail.com>
2026-02-05 09:49:16 +0530

    Azure sql script execution

--- VehicleProject/SQL_queries/vehicle_types.sql
+++ VehicleProject/SQL_queries/vehicle_types.sql
@@ -1,3 +1,2 @@
 IF NOT EXISTS (SELECT 1 FROM VehicleTypes WHERE Id = 1) INSERT INTO VehicleTypes (Id, Name) VALUES (1, 'Car');
 IF NOT EXISTS (SELECT 1 FROM VehicleTypes WHERE Id = 2) INSERT INTO VehicleTypes (Id, Name) VALUES (2, 'Bus');
-   
