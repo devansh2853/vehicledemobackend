195a8926c0e3851915f0942ed3d0b403bd6a99f7
Devansh <devanshbansal2021@gmail.com>
2026-02-10 04:59:23 +0530

    Migration created for removal of VehicleType prop

---
+++ VehicleProject/Migrations/20260209232903_RemovedPropVehicleType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedPropVehicleType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VehicleType",
+                table: "Cars");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VehicleType",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
